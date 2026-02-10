88db4c7e89eb34adba382266faadd6f4341a3804
Váradi János <82042795+janiegyketto@users.noreply.github.com>
2026-02-10 09:01:59 +0100

    kesz sql

---
+++ calander.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2026. Feb 10. 09:01
+-- Kiszolgáló verziója: 10.4.28-MariaDB
+-- PHP verzió: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `calander`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `esemenyek`
+--
+
+CREATE TABLE `esemenyek` (
+  `id` int(11) NOT NULL,
+  `cim` varchar(100) NOT NULL,
+  `leiras` varchar(250) NOT NULL,
+  `kezdet` datetime NOT NULL,
+  `veg` datetime NOT NULL,
+  `hely` varchar(50) NOT NULL,
+  `szin` enum('Zöld','Kék','Piros','Lila','Sárga') NOT NULL,
+  `letrehozva` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `profil`
+--
+
+CREATE TABLE `profil` (
+  `id` int(11) NOT NULL,
+  `nev` varchar(50) NOT NULL,
+  `felh_nev` varchar(55) NOT NULL,
+  `email` varchar(55) NOT NULL,
+  `jelszo_hash` varchar(255) NOT NULL,
+  `reg_datum` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `esemenyek`
+--
+ALTER TABLE `esemenyek`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `profil`
+--
+ALTER TABLE `profil`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `in_felh_nev` (`felh_nev`),
+  ADD UNIQUE KEY `in_email` (`email`) USING BTREE;
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `esemenyek`
+--
+ALTER TABLE `esemenyek`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `profil`
+--
+ALTER TABLE `profil`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `esemenyek`
+--
+ALTER TABLE `esemenyek`
+  ADD CONSTRAINT `esemenyek_ibfk_1` FOREIGN KEY (`id`) REFERENCES `profil` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
