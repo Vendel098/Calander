439f8510b4d10ecb17c8fe507ab8bb6c5af9dbc9
sandorvass <113939644+sandorvass@users.noreply.github.com>
2026-02-10 09:42:53 +0100

    Add files via upload

--- homepage.php
+++ homepage.php
@@ -1,87 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="hu">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="Calander-main/homepage.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Kalendárium</title>
+<link rel="stylesheet" href="homepage.css">
+<link rel="stylesheet" href="homepage.js">
+
 </head>
 <body>
-        <h2  style="color: orange;">
-        January 2025
-    </h2>
-    <br />
-    <table cellpadding="12">   
-        <caption>
-        <thead>
-            <tr> 
-                <th>Sun</th>
-                <th>Mon</th>
-                <th>Tue</th>
-                <th>Wed</th>
-                <th>Thu</th>
-                <th>Fri</th>
-                <th>Sat</th>
-            </tr>
-        </thead>
-        
-        <tbody>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>1</td>
-                <td>2</td>
-            </tr>
-            <tr></tr>
-            <tr>
-                <td>3</td>
-                <td>4</td>
-                <td>5</td>
-                <td>6</td>
-                <td>7</td>
-                <td>8</td>
-                <td>9</td>
-            </tr>
-            <tr>
-                <td>10</td>
-                <td>11</td>
-                <td>12</td>
-                <td>13</td>
-                <td>14</td>
-                <td>15</td>
-                <td>16</td>
-            </tr>
-            <tr>
-                <td>17</td>
-                <td>18</td>
-                <td>19</td>
-                <td>20</td>
-                <td>21</td>
-                <td>22</td>
-                <td>23</td>
-            </tr>
-            <tr>
-                <td>24</td>
-                <td>25</td>
-                <td>26</td>
-                <td>27</td>
-                <td>28</td>
-                <td>29</td>
-                <td>30</td>
-            </tr>
-            <tr>
-                <td>31</td>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-                <td>4</td>
-                <td>5</td>
-                <td>6</td>
-            </tr>
-        </tbody>
-    </table>
+
+<div class="calendar">
+<div class="calendar-header">
+<button onclick="prevMonth()"><</button>
+<h2 id="monthYear"></h2>
+<button onclick="nextMonth()">></button>
+</div>
+
+<div class="weekdays">
+<div>H</div><div>K</div><div>Sze</div><div>Cs</div>
+<div>P</div><div>Szo</div><div>V</div>
+</div>
+
+<div class="days" id="days"></div>
+</div>
+
+
+
 </body>
 </html>
